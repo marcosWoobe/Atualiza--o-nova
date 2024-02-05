4f4ea5a6142d662fa44c34f355662a0c93c9d2e7
Marcos Rodrigues <marcos@upflow.me>
2024-02-05 19:59:23 -0300

    b

--- src/house.h
+++ src/house.h
@@ -73,7 +73,7 @@ class AccessList
 		void getList(std::string& _list) const;
 
 	private:
-		typedef std::tr1::unordered_set<uint32_t> PlayerList;
+		typedef boost::unordered_set<uint32_t> PlayerList;
 		typedef std::list<std::pair<uint32_t, int32_t> > GuildList;
 		typedef std::list<std::string> ExpressionList;
 		typedef std::list<std::pair<boost::regex, bool> > RegExList;
